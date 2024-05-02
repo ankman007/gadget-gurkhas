c35538b98a5bdbdbcec23bd6ebe9e59915c84a67
ankman007 <ankeetpoudel6@gmail.com>
2024-05-02 17:26:28 +0545

    post-social-media-buttons-added

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -4,6 +4,8 @@ import Navigation from '@/app/components/Navigation';
 import Header from '@/app/components/LogoBanner';
 import Footer from '@/app/components/Footer';
 import { useEffect, useState } from 'react';
+import { TwitterShareButton, FacebookShareButton, EmailShareButton, RedditShareButton, TelegramShareButton, TumblrShareButton} from 'react-share';
+import { TwitterIcon, FacebookIcon, EmailIcon, RedditIcon, TelegramIcon, TumblrIcon} from 'react-share';
 
 export default function MyComponent(){
     const pathName = usePathname();
@@ -42,6 +44,32 @@ export default function MyComponent(){
                         index === 0 ? post.body : ` ${post.body}`
                     ))}
                 </p>
+                <div className='container mx-auto m-4 justify-start items-start gap-2 mb-2'>
+                <h3 className='text-lg font-extrabold mb-2'>Share on social media: </h3>
+                <ul className='flex gap-4'>
+                    <li className=''>
+                        <TwitterShareButton url={pathName} title={post.title}><TwitterIcon size={32} round={true} /></TwitterShareButton>
+                    </li>
+                    <li>
+                        <FacebookShareButton url={pathName} title={post.title}><FacebookIcon size={32} round={true} /></FacebookShareButton>    
+                    </li>
+                    <li>
+                        <EmailShareButton url={pathName} title={post.title}><EmailIcon size={32} round={true} /></EmailShareButton>
+
+                    </li>
+                    <li>
+                        <RedditShareButton url={pathName} title={post.title}><RedditIcon size={32} round={true} /></RedditShareButton>
+
+                    </li>
+                    <li>
+                        <TelegramShareButton url={pathName} title={post.title}><TelegramIcon size={32} round={true} /></TelegramShareButton>
+
+                    </li>
+                    <li>
+                        <TumblrShareButton url={pathName} title={post.title}><TumblrIcon size={32} round={true} /></TumblrShareButton>
+                    </li>
+                </ul>
+                </div>
 
             </div>
             <Footer/>
