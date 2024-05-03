b27176546894db38214ae44b3b04ba0248aca13a
Aayush Gauutam <professional.atus6693@gmail.com>
2024-05-03 20:29:00 +0545

    typo fixed

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -13,22 +13,23 @@ interface ContactDetails {
 }
 
 export default function ContactUsPage() {
-  const [ contactDetails, setContactDetails ] = useState<ContactDetails>({
-    name: '',
-    email: '',
-    message: '',
+  const [contactDetails, setContactDetails] = useState<ContactDetails>({
+    name: "",
+    email: "",
+    message: "",
     submitted: false,
-    error: '';
+    error: "",
   });
 
+  const handleFormCange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setContactDetails({ ...contactDetails, [e.target.name]: e.target.value });
+  };
 
-  const handleFormCange = (e: React.ChangeEvent<HTMLFormInputElement | HTMLTextAreaElement>) => {
-    setContactDetails({...contactDetails, [e.target.name]: e.target.value });
-  }
-
-  const handleFormSubmission = (e: React.FormEvent<HTMLFormEvent>) => {
+  const handleFormSubmission = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(contactDetails)
+    console.log(contactDetails);
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(contactDetails.email)) {
@@ -37,81 +38,96 @@ export default function ContactUsPage() {
     }
 
     setContactDetails({
-      name: '', 
-      email: '', 
-      message: '', 
+      name: "",
+      email: "",
+      message: "",
       submitted: true,
-      error: '',
+      error: "",
     });
-  };  
+  };
 
   return (
     <>
-
-    <Navigation />
+      <Navigation />
       <Header />
       <div className="container mx-auto my-8 p-8 bg-gray-100 rounded-xl">
         <h2 className="text-3xl font-bold mb-4 text-center">Contact Us</h2>
         {!contactDetails.submitted ? (
+          <form className="max-w-md mx-auto" onSubmit={handleFormSubmission}>
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-sm font-semibold mb-2"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                placeholder="Your name"
+                required
+                value={contactDetails.name}
+                onChange={handleFormCange}
+              />
+            </div>
 
-        <form className="max-w-md mx-auto" onSubmit={handleFormSubmission}>
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-sm font-semibold mb-2">
-              Name
-            </label>
-            <input 
-              type="text" 
-              id="name"
-              name="name" 
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" 
-              placeholder="Your name" 
-              required 
-              value={contactDetails.name} 
-              onChange={handleFormCange}/>
-          </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                placeholder="Your email"
+                required
+                value={contactDetails.email}
+                onChange={handleFormCange}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-semibold mb-2">Email</label>
-            <input 
-              type="email" 
-              id="email" 
-              name="email" 
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" 
-              placeholder="Your email" 
-              required 
-              value={contactDetails.email} 
-              onChange={handleFormCange}/>
-          </div>
+            <div className="mb-4">
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold mb-2"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows={4}
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg resize-none focus:outline-none focus:border-blue-500"
+                placeholder="Your message"
+                required
+                value={contactDetails.message}
+                onChange={handleFormCange}
+              ></textarea>
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="message" className="block text-sm font-semibold mb-2">Message</label>
-            <textarea 
-              id="message" 
-              name="message" 
-              rows={4}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg resize-none focus:outline-none focus:border-blue-500" 
-              placeholder="Your message" 
-              required 
-              value={contactDetails.message} 
-              onChange={handleFormCange}></textarea>
-          </div>
-
-          <div className="text-center">
-            <button 
-              type="submit" 
-              className="bg-blue-900 text-white font-bold py-2 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
-              Submit
-            </button>
-          </div>
-
-        </form>
-        ) : ( 
+            <div className="text-center">
+              <button
+                type="submit"
+                className="bg-blue-900 text-white font-bold py-2 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        ) : (
           <p className="container flex mx-auto justify-center">
-            Thank your for you message. We will get back to you as soon as possible.
-          </p> 
-        ) }
+            Thank your for you message. We will get back to you as soon as
+            possible.
+          </p>
+        )}
       </div>
       <Footer />
     </>
-  )
+  );
 }
