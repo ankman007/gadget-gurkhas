5c24f5a70b4dfa10e01e1bea9f3025f5b7f8accf
Aayush Gauutam <professional.atus6693@gmail.com>
2024-05-03 15:41:45 +0545

    json fake post+image config

--- src/app/components/OfferCards/index.tsx
+++ src/app/components/OfferCards/index.tsx
@@ -1,14 +1,11 @@
 import OfferCard from "../OfferCard";
 
-
-
 export default function OfferCards() {
   return (
-    <div className="flex justify-center gap-10 h-80 mb-4  ">
-      <OfferCard offerValue={"Top Deals This Week"}/>
-      <OfferCard offerValue={"Top Head To Head Product Comparisons"}/>
-      <OfferCard offerValue={"Full Length Product Reviews"}/>
-
+    <div className="flex justify-center gap-10 h-80 m-20">
+      <OfferCard offerValue={"Top Deals This Week"} />
+      <OfferCard offerValue={"Top Head To Head Product Comparisons"} />
+      <OfferCard offerValue={"Full Length Product Reviews"} />
     </div>
   );
 }
